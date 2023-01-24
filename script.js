960d6a610732981441821fc6579085fdef58f8bb
ElDiGio <57814202+eldigio@users.noreply.github.com>
2023-01-24 11:53:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
 'use strict';
-const MAX = 20;
-const guessInput = document.querySelector('.guess');
-const numberToGuess = document.querySelector('.number');
+
+const initColor = '#222';
+const winColor = '#60b347';
+let score = 20;
+let highScore = 0;
+
+const button = document.querySelector('.check');
+const again = document.querySelector('.again');
+const input = document.querySelector('.guess');
 const message = document.querySelector('.message');
-const score = document.querySelector('.score');
-const btnCheck = document.querySelector('.check');
+const number = document.querySelector('.number');
+const scoreValue = document.querySelector('.score');
+const highScoreValue = document.querySelector('.highscore');
 
-const randNum = Math.floor(Math.random() * MAX + 1);
-console.log(randNum);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
 
-btnCheck.addEventListener('click', () => {
-  if (guessInput.value <= 0) guessInput.value = 1;
-  if (guessInput.value > randNum) {
-    message.textContent = 'Numero troppo alto';
-    score.textContent--;
+const win = mex => {
+  message.textContent = mex;
+  number.textContent = secretNumber;
+  document.body.style = `background-color: ${winColor}`;
+  if (highScore < score) highScore = score;
+};
+
+const errorOrLost = (errorMex, lostMex = 'Hai perso') => {
+  if (score > 1) {
+    score--;
+    message.textContent = `Numero troppo ${errorMex}`;
+    scoreValue.textContent = score;
+  } else {
+    message.textContent = lostMex;
+    scoreValue.textContent = 0;
   }
-  if (guessInput.value < randNum) {
-    message.textContent = 'Numero troppo basso';
-    score.textContent--;
+};
+
+button.addEventListener('click', () => {
+  const guess = Number(input.value);
+  if (!guess) message.textContent = 'Nessun numero inserito';
+  else if (guess === secretNumber) {
+    win('Numero corretto');
+  } else if (guess > secretNumber) {
+    errorOrLost('alto');
+  } else if (guess < secretNumber) {
+    errorOrLost('basso');
   }
-  message.textContent = 'Esatto';
 });
+
+// reset score = 20, guessNumber, message, bg-color
+// generate new secretNumber
+// clear input
+
+const resetInterface = () => {
+  score = 20;
+  scoreValue.textContent = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  console.log(secretNumber);
+  number.textContent = '?';
+  input.value = '';
+  message.textContent = 'Inizia ad indovinare...';
+  document.body.style = `background-color: ${initColor}`;
+  highScoreValue.textContent = highScore;
+};
+
+again.addEventListener('click', resetInterface);
